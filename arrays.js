df66c5a6dcef946616d23e5353ac44804f9bd06d
Elizabeth Njoroge <njorogewambuielizabeth@gmail.com>
2023-04-19 10:48:13 +0300

    added fizz quiz

--- arrays.js
+++ arrays.js
@@ -153,4 +153,31 @@ function getCount(str) {
 console.log(getCount("virginia"));
 console.log(getCount("fly"));
 
+// Given a range of numbers from 0 to 100, console”Fizz” if 
+//the numbers are divisible by 3,
+//“Buzz” if the numbers are divisible by 5, and “FizzBuzz” 
+//if divisible by both 3 and 5.
+
+//div by 3= Fizz
+//div by 5 = Buzz
+//div by 3 and 5 = FizzBuzz
+//begin by div by both
+
+
+
+for(let x = 0; x <= 100; x++){
+    if(x % 3 === 0 && x % 5 === 0){
+        console.log("FizzBuzz");
+    }
+    else if(x % 3 === 0){
+        console.log("Fizz");
+    }
+    else if(x % 5 === 0){
+        console.log("Buzz");
+    }
+   else{
+    console.log(x)
+   }
+}
+
 
